50ad0aa368979d8b33796d5a287a8f1252e7459c
thiagoalmeida1905 <thi.almeida1905@gmail.com>
2025-02-12 11:18:02 -0300

    feat: connection to mongodb

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import routes from './routes';
 
 class App {
     constructor() {
         this.server = express();
+        mongoose.connect('mongodb+srv://devhouse:devhouse@devhouse.yd3u5.mongodb.net/debhouse?retryWrites=true&w=majority&appName=devhouse',{
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
         this.middlewares();
         this.routes();
     }
